f05662d4890834b7fa05a18c8a173924eafcb3e3
YaroslavMolchan <yaroslav.molchan@gmail.com>
2017-03-18 16:16:21 +0200

    test telegram

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -15,8 +15,8 @@ class TelegramController extends Controller
         try {
             $bot = new Client(env('TELEGRAM_BOT_API'));
 
-            $bot->command('ping', function ($message) use ($bot) {
-                $bot->sendMessage($message->getChat()->getId(), json_encode($message));
+            $bot->command('ping', function ($message, $params) use ($bot) {
+                $bot->sendMessage($message->getChat()->getId(), json_encode($params));
             });
 
             $bot->run();
