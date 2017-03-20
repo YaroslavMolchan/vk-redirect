0a01b61019b9035627ff267a88220b12c53d2eff
Yaroslav Molchan <yaroslav.molchan@gmail.com>
2017-03-20 16:53:16 +0200

    rewrite telegram webhook

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -5,48 +5,56 @@ namespace App\Http\Controllers;
 use App\Helpers\Vk\Helper;
 use App\Helpers\Vk\Messages\Message;
 use App\Helpers\VkHelper;
+use TelegramBot\Api\BotApi;
 use TelegramBot\Api\Client;
 use TelegramBot\Api\Exception;
 use VK\VK;
 
 class TelegramController extends Controller
 {
+    const REGEXP = '/^(?:@\w+\s)?\/([^\s@]+)\s?([^\s@]+)\s?(.*)$/';
 
     public function webhook()
     {
         try {
-            $bot = new Client(env('TELEGRAM_BOT_API'));
+            $content = file_get_contents("php://input");
+            $data = json_decode($content, true);
+
+            preg_match(self::REGEXP, $data['message']['text'], $matches);
+
+            if (!isset($matches[1]) || isset($matches[2]) || isset($matches[3])) {
+                return 'Error';
+            }
 
             $api = new VK(env('VK_APP_ID'), env('VK_API_SECRET'), env('VK_ACCESS_TOKEN'));
             $vk = new Helper();
             $vk->setSender($api);
 
-            $bot->command('answer', function ($message, $user_id, $text) use ($bot, $vk) {
-                $m = new Message();
-                $m->setMessage($text);
-                $vk->setReceiverId($user_id);
-                if (!$vk->sendMessage($m)) {
-                    $bot->sendMessage($message->getChat()->getId(), 'Произошла ошибка');
-                }
-            });
+            $telegram_api = new BotApi(env('TELEGRAM_BOT_API'));
 
-            $bot->command('quote', function ($message, $message_id, $text) use ($bot, $vk) {
-                $result = app('db')->select("SELECT `message` FROM `messages` WHERE `id` = ?", [$message_id]);
+            if ($matches[1] == 'answer') {
+                $m = new Message();
+                $m->setMessage($matches[3]);
+                $vk->setReceiverId($matches[2]);
+                if (!$vk->sendMessage($m)) {
+                    $telegram_api->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка');
+                }
+            }
+            elseif ($matches[1] == 'quote') {
+                $result = app('db')->select("SELECT `message` FROM `messages` WHERE `id` = ?", [$matches[2]]);
                 if (empty($result)) {
-                    $bot->sendMessage($message->getChat()->getId(), 'Произошла ошибка. Сообщение не найдено в базе.');
+                    $telegram_api->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка. Сообщение не найдено в базе.');
                 }
                 else {
                     $result_data = json_decode($result[0]->message);
                     $m = new Message();
-                    $m->setMessage($text);
+                    $m->setMessage($matches[3]);
                     $vk->setReceiverId($result_data->user_id);
-                    if (!$vk->sendForwardedMessage($m, $message_id)) {
-                        $bot->sendMessage($message->getChat()->getId(), 'Произошла ошибка');
+                    if (!$vk->sendForwardedMessage($m, $matches[2])) {
+                        $telegram_api->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка');
                     }
                 }
-            });
-
-            $bot->run();
+            }
 
         } catch (Exception $e) {
             $e->getMessage();
