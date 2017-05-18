c22124d646b9bdfc0d9d0ea7a77c21e5bff6635b
Yaroslav Molchan <yaroslav.molchan@gmail.com>
2017-05-18 23:14:30 +0300

    refactor telegram webhook

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -11,50 +11,55 @@ use VK\VK;
 
 class TelegramController extends Controller
 {
-    const REGEXP = '/^(?:@\w+\s)?\/([^\s@]+)\s?([^\s@]+)\s?(.*)$/';
-
     public function webhook()
     {
         try {
-            $content = file_get_contents("php://input");
-            $data = json_decode($content, true);
-
             $vk_api = new VK(env('VK_APP_ID'), env('VK_API_SECRET'), env('VK_ACCESS_TOKEN'));
             $vk = new Helper();
             $vk->setSender($vk_api);
 
             $telegram = new BotApi(env('TELEGRAM_BOT_API'));
+            $bot = new \TelegramBot\Api\Client('TELEGRAM_BOT_API');
 
-            preg_match(self::REGEXP, $data['message']['text'], $matches);
+            $bot->command('answer', function ($message, ...$params) use ($bot, $telegram, $vk) {
+                $receiver_id = array_shift($params);
+                $text = implode(' ', $params);
 
-            if (!isset($matches[1]) || !isset($matches[2]) || !isset($matches[3])) {
-                return 'Error';
-            }
-
-            if ($matches[1] == 'answer') {
                 $m = new Message();
-                $m->setMessage($matches[3]);
-                $vk->setReceiverId($matches[2]);
+                $m->setMessage($text);
+                $vk->setReceiverId($receiver_id);
                 if (!$vk->sendMessage($m)) {
                     $telegram->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка');
                 }
-            } elseif ($matches[1] == 'quote') {
-                $result = app('db')->select("SELECT `message` FROM `messages` WHERE `id` = ?", [$matches[2]]);
+            });
+
+            $bot->command('quote', function ($message, ...$params) use ($bot, $telegram, $vk) {
+                $receiver = array_shift($params);
+                $text = implode(' ', $params);
+
+                $client = new Client();
+                $client->request('GET', 'https://slack.com/api/chat.postMessage?token=' . env('SLACK_API_TOKEN') . '&channel=' . $receiver . '&text=' . $text . '&as_user=true');
+            });
+
+            $bot->command('slack', function ($message, ...$params) use ($bot, $telegram, $vk) {
+                $receiver_id = array_shift($params);
+                $text = implode(' ', $params);
+
+                $result = app('db')->select("SELECT `message` FROM `messages` WHERE `id` = ?", [$receiver_id]);
                 if (empty($result)) {
                     $telegram->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка. Сообщение не найдено в базе.');
                 } else {
                     $result_data = json_decode($result[0]->message);
                     $m = new Message();
-                    $m->setMessage($matches[3]);
+                    $m->setMessage($text);
                     $vk->setReceiverId($result_data->user_id);
-                    if (!$vk->sendForwardedMessage($m, $matches[2])) {
+                    if (!$vk->sendForwardedMessage($m, $receiver_id)) {
                         $telegram->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка');
                     }
                 }
-            } elseif ($matches[1] == 'slack') {
-                $client = new Client();
-                $client->request('GET', 'https://slack.com/api/chat.postMessage?token=' . env('SLACK_API_TOKEN') . '&channel=' . $matches[2] . '&text=' . $matches[3] . '&as_user=true');
-            }
+            });
+
+            $bot->run();
 
         } catch (Exception $e) {
             $e->getMessage();
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,48 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-$app->post('/', function () use ($app) {
+$app->any('/', function () use ($app) {
 //    $content = file_get_contents("php://input");
 //    $data = json_decode($content, true);
 //    $telegram_api = new BotApi(env('TELEGRAM_BOT_API'));
 //    $telegram_api->sendMessage(env('TELEGRAM_CHAT_ID'), json_encode($data));
-    echo 'ok';
+//    echo 'ok';
+
+    $data = [
+        'update_id' => 43374693,
+        'message' => [
+            'message_id' => 797,
+            'from' => [
+                'id' => 67852056,
+                'first_name' => 'Yaroslav',
+                'last_name' => 'Molchan',
+                'username' => 'YaroslavMolchan',
+                'language_code' => 'ru'
+            ],
+            'chat' => [
+                'id' => 67852056,
+                'first_name' => 'Yaroslav',
+                'last_name' => 'Molchan',
+                'username' => 'YaroslavMolchan',
+                'type' => 'private'
+            ],
+            'date' => 1495134105,
+            'text' => '@MRedirectBot /quote 77062 12',
+            'entities' => [
+                [
+                    'type' => 'mention',
+                    'offset' => 0,
+                    'length' => 13
+                ],
+                [
+                    'type' => 'bot_command',
+                    'offset' => 0,
+                    'length' => 13
+                ]
+            ]
+        ]
+    ];
+    return redirect('https://molchan.me');
 });
 
 $app->post('/telegram/webhook', 'TelegramController@webhook');
