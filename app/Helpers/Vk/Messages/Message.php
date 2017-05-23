a1170cf58ebe09e19d326172b6dcb4fd47c20b3f
Yaroslav Molchan <yaroslav.molchan@gmail.com>
2017-05-23 22:35:34 +0300

    test

--- app/Helpers/Vk/Messages/Message.php
+++ app/Helpers/Vk/Messages/Message.php
@@ -129,9 +129,6 @@ class Message extends \App\Helpers\Message
             array_unshift($keyboard, $attachments_keyboard);
         }
 
-        print_r($keyboard);
-        die();
-
         return new InlineKeyboardMarkup($keyboard);
     }
 
