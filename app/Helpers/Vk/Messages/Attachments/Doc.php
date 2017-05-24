f470f75ec5eaa8c76ce1419a4f3e9b5a419b2eee
Yaroslav Molchan <yaroslav.molchan@gmail.com>
2017-05-24 17:56:37 +0300

    doc

--- app/Helpers/Vk/Messages/Attachments/Doc.php
+++ app/Helpers/Vk/Messages/Attachments/Doc.php
@@ -4,16 +4,16 @@ namespace App\Helpers\Vk\Messages\Attachments;
 
 class Doc extends Attachment {
 
-    public function getMethod()
-    {
-        return 'sendDocument';
-    }
+//    public function getMethod()
+//    {
+//        return 'sendDocument';
+//    }
 
-    public function getOptions()
-    {
-        return [
-            'document' => $this->item['url'],
+//    public function getOptions()
+//    {
+//        return [
+//            'document' => $this->item['url'],
 //            'caption' => $this->item['title']
-        ];
-    }
+//        ];
+//    }
 }
