1dd5fe6904763a085cfada08d4c2efab1db93c0f
Yaroslav Molchan <yaroslav.molchan@gmail.com>
2017-05-25 10:52:07 +0300

    fix audio url

--- app/Helpers/Vk/Messages/Attachments/Audio.php
+++ app/Helpers/Vk/Messages/Attachments/Audio.php
@@ -6,11 +6,21 @@ class Audio extends Attachment {
 
     public function getMethod()
     {
+        if (empty($this->item['url'])) {
+            return 'sendMessage';
+        }
+
         return 'sendAudio';
     }
 
     public function getOptions()
     {
+        if (empty($this->item['url'])) {
+            return [
+                'text' => 'Музыка: ' . $this->item['artist'] .' - ' . $this->item['title'],
+            ];
+        }
+
         return [
             'audio' => $this->item['url'],
             'caption' => $this->item['artist'] .' - ' . $this->item['title']
