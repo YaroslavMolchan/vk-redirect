f735c1b3f8d897da58725f3d845da9617d9850bb
Yaroslav Molchan <molchan@kai24.com>
2017-08-17 15:10:24 +0300

    Remove some unused code

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -93,88 +93,10 @@ class TelegramController extends Controller
                 return true;
             });
 
-//            $attachments = [
-//                'audio',
-//                'doc',
-//                'point',
-//                'photo',
-//                'sticker',
-//                'video',
-//            ];
-//
-//            foreach ($attachments as $type) {
-//                $bot->command($type, function ($message, ...$params) use ($bot, $telegram_api, $vk, $telegram, $type) {
-//                    $message_id = array_shift($params);
-//
-//                    $result = app('db')->select("SELECT `message` FROM `messages` WHERE `id` = ?", [$message_id]);
-//                    if (empty($result)) {
-//                        $telegram_api->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка. Сообщение не найдено в базе.');
-//                    } else {
-//                        $result_data = json_decode($result[0]->message, true);
-//                        $telegram->setReceiverId(env('TELEGRAM_CHAT_ID'));
-//                        if ($type == 'point') {
-//                            $attachment = new Location($result_data['geo']);
-//                            if (!$telegram->sendAttachment($attachment)) {
-//                                $telegram_api->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка');
-//                            }
-//                        }
-//                        else {
-//                            $attachments = collect($result_data['attachments'])->where('type', $type);
-//                            foreach ($attachments as $data) {
-//                                $class = '\App\Helpers\Vk\Messages\Attachments\\' . ucfirst($type);
-//                                $attachment = new $class($data);
-//                                if (!$telegram->sendAttachment($attachment)) {
-//                                    $telegram_api->sendMessage(env('TELEGRAM_CHAT_ID'), 'Произошла ошибка');
-//                                }
-//                            }
-//                        }
-//                    }
-//                });
-//            }
-//
-//            $bot->command('slack', function ($message, ...$params) use ($bot, $telegram_api, $vk) {
-//                $receiver = array_shift($params);
-//                $text = implode(' ', $params);
-//
-//                $client = new Client();
-//                $client->request('GET', 'https://slack.com/api/chat.postMessage?token=' . env('SLACK_API_TOKEN') . '&channel=' . $receiver . '&text=' . $text . '&as_user=true');
-//            });
-//
             $bot->run();
 
         } catch (Exception $e) {
             $e->getMessage();
         }
-    }
-
-    public function p($input = null, $title = null)
-    {
-        if (is_null($title)) {
-            $title=$_SERVER['REQUEST_METHOD'];
-        }
-        if (is_null($input)) {
-            $text = var_export($_REQUEST, true);
-            $input = @file_get_contents("php://input");
-        }
-//        $event = json_decode($input);
-        $url = "https://api.pushbullet.com/v2/pushes";
-        $data = [
-            'type' => 'note',
-            'title' => $title,
-            'body' => $input,
-//            'body' => $text,
-        ];
-        $data = json_encode($data);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-            "Authorization: Bearer o.n9yeGc7W6mdLVXucmwCUW3VJ8OKdAdPn",
-            "Content-Type: application/json",
-            'Content-Length: ' . strlen($data)
-        ]);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-        $result = curl_exec($ch);
-        curl_close($ch);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,56 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 $app->get('/', function () use ($app) {
-//    $content = file_get_contents("php://input");
-//    $data = json_decode($content, true);
-//    $telegram_api = new BotApi(env('TELEGRAM_BOT_API'));
-//    $telegram_api->sendMessage(env('TELEGRAM_CHAT_ID'), json_encode($data));
-//    echo 'ok';
-
-    $data = [
-        'update_id' => 43374693,
-        'message' => [
-            'message_id' => 797,
-            'from' => [
-                'id' => 67852056,
-                'first_name' => 'Yaroslav',
-                'last_name' => 'Molchan',
-                'username' => 'YaroslavMolchan',
-                'language_code' => 'ru'
-            ],
-            'chat' => [
-                'id' => 67852056,
-                'first_name' => 'Yaroslav',
-                'last_name' => 'Molchan',
-                'username' => 'YaroslavMolchan',
-                'type' => 'private'
-            ],
-            'date' => 1495134105,
-            'text' => '@MRedirectBot /quote 77062 12',
-            'entities' => [
-                [
-                    'type' => 'mention',
-                    'offset' => 0,
-                    'length' => 13
-                ],
-                [
-                    'type' => 'bot_command',
-                    'offset' => 0,
-                    'length' => 13
-                ]
-            ]
-        ]
-    ];
     return redirect('https://molchan.me');
 });
 
